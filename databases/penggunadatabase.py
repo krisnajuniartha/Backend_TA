f0bb96a6af671adfdc67a31fbcbb656d3a749f86
krisnajuniartha <gedekrisna22@gmail.com>
2025-04-30 13:05:57 +0800

    add app in main

--- databases/penggunadatabase.py
+++ databases/penggunadatabase.py
@@ -1,7 +1,5 @@
 from models.pengguna import *
 from motor.motor_asyncio import AsyncIOMotorClient
-from pymongo.mongo_client import MongoClient
-from pymongo.server_api import ServerApi
 from bson import ObjectId
 from email_validator import validate_email, EmailNotValidError
 import cloudinary
